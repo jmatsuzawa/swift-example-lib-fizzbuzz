a32db6f5aabaa62d97eb5fa335730eac6a7f0e0c
Jiro Matsuzawa <j.matsuzawa@newnormal-resource.com>
2024-10-09 15:59:10 +0900

    Add dependency on swift-testing
    swift package add-dependency https://github.com/swiftlang/swift-testing.git  --revision swift-6.0.1-RELEASE

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "FizzBuzz",
             targets: ["FizzBuzz"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-testing.git", revision: "swift-6.0.1-RELEASE"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
